9d651418d9933b5d74340549c103a865f8f3cb91
horghada20 <horghada20@gmail.com>
2026-09-22 22:30:50 +0300

    Keep command aliases consistent with prefixed trusted command

--- src/config/commands/commandAliases.js
+++ src/config/commands/commandAliases.js
@@ -1,44 +1,24 @@
-/**
- * Command aliases configuration.
- * Arabic names are message-command shortcuts for the canonical English commands.
- */
+/** Command aliases configuration. */
 export const commandAliases = {
-  bal: 'balance', money: 'balance', cash: 'balance',
-  dep: 'deposit', with: 'withdraw',
-  bet: 'gamble', give: 'pay', send: 'pay',
-  h: 'help', info: 'help',
-
-  // Moderation shortcuts
-  بان: 'ban', انبان: 'unban',
-  تايم: 'timeout', انتايم: 'untimeout',
-  mute: 'timeout', unmute: 'untimeout',
-  وارن: 'warn', وارنات: 'warnings',
-  كلير: 'purge', clear: 'purge',
-  kick: 'kick', ban: 'ban', warn: 'warn',
-  purge: 'purge', untimeout: 'untimeout',
-
-  rank: 'rank', lvl: 'rank', xp: 'rank',
-  leaderboard: 'leaderboard', lb: 'leaderboard', top: 'leaderboard',
+  bal: 'balance', money: 'balance', cash: 'balance', dep: 'deposit', with: 'withdraw',
+  bet: 'gamble', give: 'pay', send: 'pay', h: 'help', info: 'help',
+  بان: 'ban', انبان: 'unban', تايم: 'timeout', انتايم: 'untimeout', mute: 'timeout', unmute: 'untimeout',
+  وارن: 'warn', وارنات: 'warnings', كلير: 'purge', clear: 'purge',
+  kick: 'kick', ban: 'ban', warn: 'warn', purge: 'purge', untimeout: 'untimeout',
+  rank: 'rank', lvl: 'rank', xp: 'rank', leaderboard: 'leaderboard', lb: 'leaderboard', top: 'leaderboard',
   shop: 'shop', buy: 'buy', inventory: 'inventory', inv: 'inventory', items: 'inventory',
-  user: 'userinfo', avatar: 'avatar', pfp: 'avatar', icon: 'avatar',
-  bd: 'birthday', bday: 'birthday', b: 'birthday',
+  user: 'userinfo', avatar: 'avatar', pfp: 'avatar', icon: 'avatar', bd: 'birthday', bday: 'birthday', b: 'birthday',
   flip: 'flip', coin: 'flip', roll: 'roll', dice: 'roll', fight: 'fight',
-  gstart: 'gcreate', gstop: 'gend', groll: 'greroll',
-  ticket: 'ticket', t: 'ticket', new: 'ticket',
-  ver: 'verify', vadmin: 'verification', av: 'autoverify',
-  welcome: 'welcome', greet: 'greet', goodbye: 'goodbye', autorole: 'autorole',
-  calc: 'calculate', math: 'calculate', weather: 'weather', todo: 'todo', report: 'report',
-  userinfo: 'userinfo', whois: 'userinfo', ui: 'userinfo',
-  serverstats: 'serverstats', ss: 'serverstats', sstats: 'serverstats',
-  rr: 'reactroles', reactionroles: 'reactroles',
-  jtc: 'jointocreate', jointocreate: 'jointocreate',
-  np: 'nowplaying', now: 'nowplaying',
+  gstart: 'gcreate', gstop: 'gend', groll: 'greroll', ticket: 'ticket', t: 'ticket', new: 'ticket',
+  ver: 'verify', vadmin: 'verification', av: 'autoverify', welcome: 'welcome', greet: 'greet', goodbye: 'goodbye', autorole: 'autorole',
+  calc: 'calculate', math: 'calculate', weather: 'weather', todo: 'todo', report: 'report', userinfo: 'userinfo', whois: 'userinfo', ui: 'userinfo',
+  serverstats: 'serverstats', ss: 'serverstats', sstats: 'serverstats', rr: 'reactroles', reactionroles: 'reactroles',
+  jtc: 'jointocreate', jointocreate: 'jointocreate', np: 'nowplaying', now: 'nowplaying',
 };
 
 export const subcommandAliases = {
   l: 'list', ls: 'list', s: 'set', i: 'info', r: 'remove', rm: 'remove', del: 'remove', n: 'next', sc: 'setchannel',
-  a: 'add', c: 'complete', done: 'complete', d: 'complete',
-  start: 'create', stop: 'end', roll: 'reroll', add: 'add', remove: 'remove', list: 'list',
+  a: 'add', c: 'complete', done: 'complete', d: 'complete', start: 'create', stop: 'end', roll: 'reroll', add: 'add', remove: 'remove', list: 'list',
 };
 
 export function resolveCommandAlias(commandName) {
